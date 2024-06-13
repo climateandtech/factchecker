cdcae043dc324decc81963f93292b53987678213
marchburg <82476336+marchburg@users.noreply.github.com>
2024-06-13 14:53:07 +0200

    refactored so ColBERT index does not get recomputed if index file with name specified in indexer.index already exists

--- factchecker/experiments/colbert_claim_evaluation.py
+++ factchecker/experiments/colbert_claim_evaluation.py
@@ -1,7 +1,7 @@
 from factchecker.indexing.colbert_indexer import ColBERTIndexer
+from factchecker.retrieval.colbert_retriever import ColBERTRetriever
 
 def main():
-    # Define the options for each component
     indexer_options = {
         'source_directory': 'data',
         'index_name': 'colbert_index',
@@ -9,10 +9,18 @@ def main():
         'split_documents': True,
         'checkpoint': 'colbert-ir/colbertv2.0'
     }
+    retriever_options = {
+    }
 
-    # Initialize the ColBERT indexer and create the index
     indexer = ColBERTIndexer(indexer_options)
-    indexer.create_index()
+    retriever = ColBERTRetriever(indexer, retriever_options)
+
+    query = "Climate change would have happened without humans"
+
+    results = retriever.retrieve(query, top_k=5)
+
+    for result in results:
+        print(result)
 
 if __name__ == "__main__":
     main()
