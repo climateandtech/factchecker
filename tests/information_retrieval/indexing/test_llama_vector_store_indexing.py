2e480a02c9de99cc0ad745260b2ff53fc32c929e
marchburg <82476336+marchburg@users.noreply.github.com>
2024-08-23 13:02:02 +0200

    updated tests for llama vector store indexer

--- tests/information_retrieval/indexing/test_llama_vector_store_indexing.py
+++ tests/information_retrieval/indexing/test_llama_vector_store_indexing.py
@@ -3,10 +3,8 @@ from factchecker.indexing.llama_vector_store_indexer import LlamaVectorStoreInde
 
 def test_create_index_with_documents(prepare_documents):
 
-    documents = prepare_documents
-
     indexer_options = {
-        'documents': documents,
+        'documents': prepare_documents,
         'index_name': 'test_index_with_docs',
     }
     
@@ -16,10 +14,10 @@ def test_create_index_with_documents(prepare_documents):
     # Assertions
     assert indexer.index is not None, "Index should be created"
     assert indexer.documents is not None, "Documents should be loaded into the indexer"
-    assert len(indexer.documents) == len(documents), "All documents should be indexed"
+    assert len(indexer.documents) == len(prepare_documents), "All documents should be indexed"
 
 def test_create_index_from_directory(prepare_test_data_directory):
-    # Using the prepared test data directory
+
     indexer_options = {
         'source_directory': prepare_test_data_directory,
         'index_name': 'test_index_from_dir',
@@ -35,10 +33,8 @@ def test_create_index_from_directory(prepare_test_data_directory):
 
 def test_create_index_when_index_already_exists(prepare_documents):
 
-    documents = prepare_documents
-
     indexer_options = {
-        'documents': documents,
+        'documents': prepare_documents,
         'index_name': 'test_existing_index',
     }
 
@@ -51,6 +47,6 @@ def test_create_index_when_index_already_exists(prepare_documents):
     # Assertions
     assert indexer.index is not None, "Index should still exist"
     assert indexer.documents is not None, "Documents should still be loaded into the indexer"
-    assert len(indexer.documents) == len(documents), "The same number of documents should be indexed"
+    assert len(indexer.documents) == len(prepare_documents), "The same number of documents should be indexed"
 
 # TODO: Add more tests as needed for additional methods and edge cases
