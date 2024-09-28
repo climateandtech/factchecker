9f36b8b5e55400a4345ca22bd3fe73e10834e3fc
marchburg <82476336+marchburg@users.noreply.github.com>
2024-09-28 16:31:18 +0200

    added docstring to init of AbstractIndexer

--- factchecker/indexing/llama_colbert_indexer.py
+++ factchecker/indexing/llama_colbert_indexer.py
@@ -2,11 +2,6 @@ from factchecker.indexing.abstract_indexer import AbstractIndexer
 from llama_index.indices.managed.colbert import ColbertIndex
 
 
-# There is a bug with the LlamaIndex ColBERT Indexing resulting in:
-# TypeError: ColbertIndex._build_index_from_nodes() got an unexpected keyword argument 'index_name'
-# See: https://github.com/run-llama/llama_index/issues/14398 
-
-
 class LlamaColBERTIndexer(AbstractIndexer):
     def __init__(self, options=None):
         super().__init__(options)
@@ -21,8 +16,13 @@ class LlamaColBERTIndexer(AbstractIndexer):
 
     def create_index(self):
         
+        # Stop further execution if the index already exists or was loaded
         if super().create_index():
-            return  # Stop further execution if the index already exists or was loaded
+            return  
+        
+        # There is currently (28.9.2024) a bug with the LlamaIndex ColBERT Indexing using the from_documents function resulting in:
+        # TypeError: ColbertIndex._build_index_from_nodes() got an unexpected keyword argument 'index_name'
+        # See: https://github.com/run-llama/llama_index/issues/14398 
 
         self.index = ColbertIndex.from_documents(
             self.documents,
@@ -31,13 +31,13 @@ class LlamaColBERTIndexer(AbstractIndexer):
             )
 
     def load_index(self):
-        # TODO: implement
+        raise NotImplementedError("load_index() of LlamaColBERTIndexer is not yet implemented")
         pass
 
     def add_to_index(self, documents):
-        # TODO: implement
+        raise NotImplementedError("add_to_index() of LlamaColBERTIndexer is not yet implemented")
         pass
 
     def delete_from_index(self, document_ids):
-        # TODO: implement
+        raise NotImplementedError("delete_from_index() of LlamaColBERTIndexer is not yet implemented")
         pass
