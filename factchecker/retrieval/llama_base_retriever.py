547e8cf56a7aac214ce98b4c9ac374dd7276bba9
Christian <chris@devolute.org>
2025-01-10 22:35:53 +0100

    fix: Remove eager initialization and fix transformations handling

--- factchecker/retrieval/llama_base_retriever.py
+++ factchecker/retrieval/llama_base_retriever.py
@@ -3,7 +3,6 @@ from factchecker.retrieval.abstract_retriever import AbstractRetriever
 class LlamaBaseRetriever(AbstractRetriever):
     def __init__(self, indexer, options=None):
         super().__init__(indexer, options)
-        self.create_retriever()
 
     def create_retriever(self):
         super().create_retriever()
