d87356ffccf849ed9517df6be2c86b6c366cbbdc
gmguarino <gmguarino1@gmail.com>
2025-02-24 14:43:11 +0100

    Add extractor step

---
+++ factchecker/steps/extract.py
@@ -0,0 +1,62 @@
+from llama_index.core.llms import ChatMessage
+import logging
+from factchecker.core.llm import load_llm
+from dataclasses import dataclass
+from typing import List
+import json
+import os
+
+@dataclass
+class ClaimList:
+    claims: List[str]
+
+class ExtractStep:
+    """
+    A preprocessing step in the fact-checking process that examines the text being classified and splits it into multiple claims if necessary. 
+    
+    This class then returns a list of claims to be analysed
+    """
+
+    def __init__(self, llm=None, options=None):
+        """
+        Initialize an ExtractStep instance.
+
+        Args:
+            llm: Language model instance to use for evaluation. If None, loads default model.
+            options (dict, optional): Configuration options for the advocate step including:
+                - system_prompt_template: Template for system information
+                - format_prompt: Instructions for response format
+        """
+        self.llm = llm if llm is not None else load_llm()
+        self.options = options if options is not None else {}
+        self.claim_prompt_template = self.options.pop('evidence_prompt_template', "Split the following text into claims: {text}")
+        self.system_prompt_template = self.options.pop('system_prompt_template', "You are an assistant splitting text into indivudual claims.")
+        self.format_prompt = self.options.pop("format_prompt", "Return as a list of claims [<claim_1>, <claim_2>, <claim_3>, ...]")
+        
+    def extract_cliams(self, text):
+        """
+        Evaluate a claim based on gathered evidence using the language model.
+
+        Args:
+            text (str): The text being split into multiple claims.
+
+        Returns:
+            claims (ClaimList): Data structure containing the list of claims extracted.
+        """
+        system_prompt_with_text = self.system_prompt_template.format(text=text)
+        format_prompt = self.format_prompt
+        combined_prompt = f"{system_prompt_with_text}\n{format_prompt}"
+        messages = [
+            ChatMessage(role="system", content=system_prompt_with_text),
+            ChatMessage(role="user", content=combined_prompt)
+        ]
+
+        response = self.llm.chat(messages, **self.additional_options)
+        response_content = response.message.content.strip()
+        try:
+            claims = ClaimList(json.loads(response_content))
+            return claims.claims
+        except Exception as e:
+            logging.warning(f"Unexpected response content: {response_content}")
+            logging.warning(f"Caught exception {e}, will return full text.")
+            return text
